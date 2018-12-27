5f45a8a18fe221ba83acf7baf3b41c37d7189e71
skhanna1011 <skhanna1011@gmail.com>
2018-12-27 22:31:58 +0530

    Adds root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   namespace :api, format: :json do
     namespace :v1 do
       resources :products, only: [:index, :create, :show, :update, :destroy]
@@ -6,7 +7,9 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
+    root to: "products#index"
     resources :products, only: [:index, :destroy, :edit, :update]
   end
+  root to: "admin/products#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
